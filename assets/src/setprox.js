7ed8d28bc170fc39e01a324dac5d90cd0a164989
Yi Ren <jamesren1998@gmail.com>
2020-02-27 13:26:08 -0500

    :bug: proxy error..
    proxy error..

--- assets/src/setprox.js
+++ assets/src/setprox.js
@@ -1,4 +1,4 @@
-const proxy = require('http-proxy-middleware');
+import proxy from 'http-proxy-middleware';
 
 module.exports = app => {
   app.use(proxy('/api', { target: 'http://localhost:1337/' }));
